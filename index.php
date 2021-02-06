dfde996e7e5bd86806c500959f101b2f3d361f51
Ankit Aakodiya <64556379+ankit2142@users.noreply.github.com>
2021-02-06 10:44:16 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name : <input type="text" placeholder="Username"  name="username"><br><br>
+Email : <input type="text" placeholder="password"  name="password"><br><br>
+<input type="submit" ,name="submit">
+</form>
+
+</body>
+</html>
